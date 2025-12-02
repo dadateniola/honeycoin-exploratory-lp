4fee45ae73634a91a8c286b2a6bd151b53acfab8
Dada Teniola <emmatenny2004@gmail.com>
2025-12-02 17:57:55 +0100

    feat(about): enhance animations and add overlay for About section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useRef } from "react";
+
 // Images
 import { AboutArrow } from "@/components/svg/svg";
 
@@ -10,14 +14,54 @@ import {
   SECTION_HEADING_CLASS,
 } from "@/components/global/data";
 
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import AltCTA from "@/components/cta/alt-cta";
+import DrawSVGPlugin from "gsap/DrawSVGPlugin";
 import { typedEntries } from "@/utils/typedEntries";
 import { BRAND_LOGO_CLASS } from "@/components/about/data";
 import PinnedUntilFooter from "@/components/footer/pinned-until-footer";
 
 const About = () => {
+  // Refs
+  const contentRef = useRef<HTMLDivElement>(null);
+  const overlayRef = useRef<HTMLDivElement>(null);
+  const brandTitleRef = useRef<HTMLHeadingElement>(null);
+  const brandCardsRef = useRef<(HTMLDivElement | null)[]>([]);
+
+  // Animations
+  useGSAP(() => {
+    gsap.registerPlugin(DrawSVGPlugin);
+
+    const content = contentRef.current;
+    const overlay = overlayRef.current;
+    const brandCards = brandCardsRef.current;
+    const brandTitle = brandTitleRef.current;
+    const arrorPath = document.getElementById("about-arrow-path");
+
+    const tl = gsap.timeline({ defaults: { ease: "power2.out" } });
+
+    tl.set(content, { autoAlpha: 0 })
+      .set(brandTitle, { yPercent: 100 })
+      .set(brandCards, { yPercent: 100 })
+      .set(arrorPath, { drawSVG: 0 });
+
+    tl.to(overlay, { scaleY: 0, ease: "expo.inOut", delay: 0.5, duration: 2 })
+      .to(content, { autoAlpha: 1 }, "-=0.5")
+      .to(arrorPath, { drawSVG: "100%", duration: 1 }, "<")
+      .to(brandTitle, { yPercent: 0 })
+      .to(brandCards, { yPercent: 0, stagger: 0.1 }, "<");
+
+    tl.set(overlay, { display: "none", pointerEvents: "none" });
+  }, []);
+
   return (
     <>
+      <div
+        ref={overlayRef}
+        className="fixed inset-0 w-screen h-screen bg-primary origin-bottom z-10" // Original state
+      ></div>
+
       <PinnedUntilFooter>
         <section id="about-us">
           <div className="relative w-full bg-background">
@@ -33,7 +77,10 @@ const About = () => {
             <div className="relative p-10 pt-[200px] custom-flex-col gap-[60px]">
               <div className="flex gap-8">
                 <div className="flex-1 min-w-0">
-                  <div className="w-full max-w-[800px] flex flex-col gap-4">
+                  <div
+                    ref={contentRef}
+                    className="w-full max-w-[800px] flex flex-col gap-4"
+                  >
                     <h1
                       className={clsx(
                         "w-full max-w-[477px]",
@@ -75,17 +122,24 @@ const About = () => {
               </div>
 
               <div className="custom-flex-col gap-10">
-                <p className="text-white/90 text-xl font-semibold -tracking-[0.2px] leading-[150%]">
-                  BRANDS WE&apos;VE WORKED WITH:
-                </p>
+                <div className="overflow-hidden">
+                  <p
+                    ref={brandTitleRef}
+                    className="text-white/90 text-xl font-semibold -tracking-[0.2px] leading-[150%]"
+                  >
+                    BRANDS WE&apos;VE WORKED WITH:
+                  </p>
+                </div>
 
                 <div className="grid grid-cols-4 gap-3">
                   {typedEntries(BRAND_LOGOS_MAP).map(([key, Logo], index) => (
-                    <div
-                      key={index}
-                      className="w-full h-[200px] p-5 custom-flex-center bg-about-card-bg rounded-[10px] overflow-hidden"
-                    >
-                      <Logo className={BRAND_LOGO_CLASS[key]} />
+                    <div key={index} className="size-full overflow-hidden">
+                      <div
+                        ref={(el) => void (brandCardsRef.current[index] = el)}
+                        className="w-full h-[200px] p-5 custom-flex-center bg-about-card-bg rounded-[10px] overflow-hidden"
+                      >
+                        <Logo className={BRAND_LOGO_CLASS[key]} />
+                      </div>
                     </div>
                   ))}
                 </div>
